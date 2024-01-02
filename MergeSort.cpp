6853a215e4accea07318d7fbc021845460537c65
Lakshmi-PrabhaS <36378735+Lakshmi-PrabhaS@users.noreply.github.com>
2024-01-02 13:45:29 +0530

    Update MergeSort.cpp

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
 
-void PrintArray(int *array, int n) 
-{
+void PrintArray(int *array, int n) {
   for (int i = 0; i < n; ++i)
     std::cout << array[i] << " " << std::flush;
   std::cout << std::endl;
 }
 
-void Merger(int arr[], int lo, int  mi, int hi)
-{
+void Merger(int arr[], int lo, int  mi, int hi) {
     int *temp = new int[hi-lo+1];//temporary merger array
     int i = lo, j = mi + 1;//i is for left-hand,j is for right-hand
     int k = 0;//k is for the temporary array
-    while(i <= mi && j <=hi)
-    {
+    while(i <= mi && j <=hi) {
         if(arr[i] <= arr[j])
             temp[k++] = arr[i++];
         else
@@ -32,11 +29,9 @@ void Merger(int arr[], int lo, int  mi, int hi)
     delete []temp;
 }
 
-void MergeSortHelper(int arr[], int lo, int hi)
-{
+void MergeSortHelper(int arr[], int lo, int hi) {
     int mid;
-    if(lo < hi)
-    {
+    if(lo < hi) {
         mid = (lo + hi) >> 1;
         MergeSortHelper(arr, lo, mid);
         MergeSortHelper(arr, mid+1, hi);
@@ -44,13 +39,11 @@ void MergeSortHelper(int arr[], int lo, int hi)
     }
 }
 
-void MergeSort(int arr[], int arr_size)
-{
+void MergeSort(int arr[], int arr_size) {
     MergeSortHelper(arr, 0, arr_size-1);
 }
 
-int main() 
-{
+int main() {
   int array[] = {94, 42, 50, 95, 333, 65, 54, 456, 1, 1234};
   int n = sizeof(array)/sizeof(array[0]);
 
