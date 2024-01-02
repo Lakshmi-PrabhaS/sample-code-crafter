0434393e94f9d29e50a15d5b1eb0638054c9c286
Lakshmi-PrabhaS <36378735+Lakshmi-PrabhaS@users.noreply.github.com>
2024-01-02 21:41:15 +0530

    Create custom_check.cpp

---
+++ custom_check.cpp
@@ -0,0 +1,48 @@
+// custom_check.cpp
+#include <clang/AST/ASTConsumer.h>
+#include <clang/AST/RecursiveASTVisitor.h>
+#include <clang/Frontend/FrontendActions.h>
+#include <clang/Tooling/Tooling.h>
+
+class VariableNamingVisitor
+  : public clang::RecursiveASTVisitor<VariableNamingVisitor> {
+public:
+  bool VisitVarDecl(clang::VarDecl *varDecl) {
+    std::string varName = varDecl->getNameAsString();
+    if (varDecl->isConstexpr()) {
+      for (char c : varName) {
+        if (!std::isupper(c) && c != '_') {
+          llvm::errs() << "Constant name is not all uppercase or does not use underscores: " << varName << "\n";
+          break;
+        }
+      }
+    } else if (!varName.empty() && !std::islower(varName[0])) {
+      llvm::errs() << "Variable name does not start with lower case: " << varName << "\n";
+    }
+    return true;
+  }
+};
+
+class VariableNamingConsumer : public clang::ASTConsumer {
+public:
+  virtual void HandleTranslationUnit(clang::ASTContext &Context) {
+    visitor.TraverseDecl(Context.getTranslationUnitDecl());
+  }
+
+private:
+  VariableNamingVisitor visitor;
+};
+
+class VariableNamingAction : public clang::ASTFrontendAction {
+public:
+  virtual std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(
+    clang::CompilerInstance &Compiler, llvm::StringRef InFile) {
+    return std::unique_ptr<clang::ASTConsumer>(
+      new VariableNamingConsumer());
+  }
+};
+
+int main(int argc, char **argv) {
+  clang::tooling::runToolOnCode(new VariableNamingAction, argv[1]);
+  return 0;
+}
