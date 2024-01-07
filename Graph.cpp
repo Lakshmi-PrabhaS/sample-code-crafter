fa8059a98a92414539fd2250c333cbc27209b40a
Lakshmi-PrabhaS <36378735+Lakshmi-PrabhaS@users.noreply.github.com>
2024-01-08 02:06:51 +0530

    Added IsBipartile() to Graph class

--- Graph.cpp
+++ Graph.cpp
@@ -98,4 +98,33 @@ class Graph {
 
 		return matrix;
 	}
+
+	bool isBipartite(vector<vector<int>>& graph) {
+		int n = graph.size();
+		vector<int>color(n, -1);
+	
+		for (int i = 0; i < n; i++) {
+			if (color[i] != -1) continue;
+	
+			color[i] = 1;
+			queue<int>q;
+			q.push(i);
+	
+			while (!q.empty()) {
+				int t = q.front();
+				q.pop();
+	
+				for (int j = 0; j < graph[t].size(); j++) {
+					if (color[graph[t][j]] == -1) {
+						color[graph[t][j]] = 1-color[t];
+						q.push(graph[t][j]);
+					} else if (color[graph[t][j]] == color[t]) {
+						return false;
+					}
+				}
+			}
+		}
+	
+		return true;
+	}
  };
